d4bfe54bd44ae8b3967591d7fd44e882c11e9996
ecly <ecly@mailbox.org>
2018-12-09 17:46:08 +0100

    move evaluation to own module and add initial gold standard evaluator

---
+++ evaluate.py
@@ -0,0 +1,115 @@
+import sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import data
+import train
+# DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cpu")
+
+def compute_cosine(tensor1, tensor2, device=DEVICE):
+    """
+    Compute cosine similarity between two pytorch tensors
+    Returns a regular python float where 1.0 means identical
+    tensors and 0.0 means orthoganal tensors.
+    """
+    tensor1 = tensor1.to(device)
+    tensor2 = tensor2.to(device)
+    return F.cosine_similarity(tensor1, tensor2, dim=0).item()
+
+
+def evaluate_gre(model, adj_model, device=DEVICE, gre=None):
+    """
+    Evaluate the given model according to GRE
+    question set. The given adj_model is needed to compute
+    embeddings for the GRE adjectives.
+
+    Optionally takes a loaded GRE dataset to avoid loading
+    multiple times, if evaluation is ran repeatedly.
+    """
+    gre_data = data.load_gre_test_set(adj_model) if gre is None else gre
+    right = []
+    wrong = []
+    for test in gre_data:
+        adj_str, options, answer = test
+        adj = adj_model.adj_from_name(adj_str)
+        gate = data.find_gate_vector(adj, adj_model)
+
+        x, y = adj.embedding.to(device), gate.to(device)
+        ant_pred = model(x, y)
+
+        most_similar = 0
+        most_similar_word = ""
+        for opt_str in options:
+            opt = adj_model.adj_from_name(opt_str)
+            similarity = compute_cosine(ant_pred, opt.embedding)
+            if similarity > most_similar:
+                most_similar = similarity
+                most_similar_word = opt_str
+
+        if most_similar_word == answer:
+            right.append(test)
+        else:
+            wrong.append(test)
+
+
+    return right, wrong
+
+def evaluate_gold_standard(model, adj_model, device=DEVICE, gold=None):
+    """
+    Evaluate the given model according to the gold standard.
+    The given adj_model is needed to compute embeddings for
+    the gold standard adjectives.
+    """
+    gold_data = data.load_gold_standard(adj_model) if gold is None else gold
+    right = []
+    wrong = []
+    for adj_str, antonyms in gold_data.items():
+        adj = adj_model.adj_from_name(adj_str)
+        gate = data.find_gate_vector(adj, adj_model)
+        x, y = adj.embedding.to(device), gate.to(device)
+        ant_pred = model(x, y)
+        ant_name = adj_model.adj_from_vector(ant_pred).name
+        if ant_name in antonyms:
+            right.append((adj_str, ant_name))
+        else:
+            wrong.append((adj_str, ant_name))
+
+    return right, wrong
+
+
+def evaluate(model, adj_model, device=DEVICE):
+    """
+    Evaluates the given model on both GRE 5 option questions
+    and on the gold standard. Results are printed to stdout.
+    """
+    with torch.set_grad_enabled(False):
+        gre_data = data.load_gre_test_set(adj_model)
+        gre_right, _gre_wrong = evaluate_gre(model, adj_model, device, gre_data)
+        gre_acc = len(gre_right) / len(gre_data)
+        print("GRE question accuracy: %.2f" % gre_acc)
+
+        gold_data = data.load_gold_standard(adj_model)
+        gold_right, _gold_wrong = evaluate_gold_standard(model, adj_model, device, gold_data)
+        gold_acc = len(gold_right) / len(gold_data)
+        print("Gold standard accuracy: %.2f" % gold_acc)
+
+
+def main(model_path):
+    """
+    Loads the adj_model and EncoderDecoder model from a checkpoint.
+    The EncoderDecoder model is then evaluated on both GRE question
+    answer set and on the gold standard antonym prediction task.
+    """
+    adj_model = data.build_adj_model()
+    model, _optimizer = train.initialize_model(model_path, DEVICE)
+    model.eval()
+    evaluate(model, adj_model)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Expected path for model as argument")
+        sys.exit(1)
+    else:
+        MODEL_PATH = sys.argv[1]
+        main(MODEL_PATH)
